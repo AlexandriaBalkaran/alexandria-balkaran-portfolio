68ddd4b538a8df18669bae619001c80648d77a5b
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-04-08 15:14:52 -0400

    add AboutSection

---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,14 @@
+import "./AboutSection.scss";
+
+function AboutSection() {
+
+  return (
+    <>
+    <h2>About</h2>
+    {/* <p>After traveling the world I've decided to change my career</p> */}
+    <p>From Sales & Marketing to Software Engineering—rewiring my career to build, create, and solve. I love tackling challenges, learning fast, and turning ideas into reality. Let’s make something great!</p>
+    </>
+  );
+}
+
+export default AboutSection;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import "./Homepage.scss";
+import AboutSection from "../../components/AboutSection/AboutSection";
 
 function Homepage() {
 
   return (
     <>
+      <AboutSection></AboutSection>
     </>
   );
 }
