037d29455acb3c9752d9c5ca2fc5fe14255f7f27
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-04-16 15:39:18 -0400

    nav-bar flex

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,16 +8,19 @@ function NavBar() {
 
   return (
     <>
-      <div>
-        <p onClick={() => scrollToSection("about")}>About</p>
-        <p onClick={() => scrollToSection("projects")}>Projects</p>
-        <p onClick={() => scrollToSection("career")}>Career</p>
-      </div>
-      <div>
-        <p>Contact</p>
+      <div className="nav-bar__container">
+        <div className="nav-bar__button">
+          <p onClick={() => scrollToSection("about")}>About</p>
+          <p onClick={() => scrollToSection("projects")}>Projects</p>
+          {/* <p onClick={() => scrollToSection("career")}>Career</p> */}
+        </div>
+        <div className="nav-bar__button-contact">
+          <p>Contact</p>
+        </div>
       </div>
     </>
   );
 }
 
 export default NavBar;
+
